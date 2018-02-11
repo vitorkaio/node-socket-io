094ad0837012254e66ae0add8abbbe08891e47de
Vítor <vitorcaiodepaula@gmail.com>
2018-02-11 18:17:30 -0200

    go

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+import ApiFirebaseAcess from './services/apiFirebaseAcess';
 
-let allClients = [];
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+const allClients = [];
+let rotaAutal = null;
+let subs = null;
 
 app.get('/', (req, res) => {
   res.send('server is running');
@@ -14,29 +18,65 @@ io.on("connection", (client) => {
 
   // Fica escutando o evento getText que devolve o texto corrente da url passada.
   client.on('getText', (url) => {
+    let obs = {
+      next: (data) => {
+        console.log(data);
+        // Emite para o cliente qualquer alteração que houver no firebase.
+        if(data !== null)
+          client.emit('getReactApp', data);
+        else
+          client.emit('getReactApp', false);
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    }
+    verificaRota(url);
     console.log(url);
 
-    // Emite para o cliente qualquer alteração que houver no firebase.
-    client.emit('sendReactApp', 'Tomara que eu não tenha feito cagada.');
+    rotaAutal = url;
+    subs = ApiFirebaseAcess.getRotaTexto(url).subscribe(obs);
+  });
+
+  // Insere um texto em uma rota.
+  client.on('postText', (url, msg) => {
+    console.log(url, 'msg: ' + msg);
+    ApiFirebaseAcess.postRotaTexto(url, msg).then(data => {
+      console.log(data);
+      client.emit(data);
+    }).catch(err => {
+      client.emit(false);
+    });
   });
 
   // ************************* Fecha o servidor *************************
   client.on('close', () => {
     console.log('user disconnected', client.id);
-    var i = allClients.indexOf(client);
+    const i = allClients.indexOf(client);
     allClients.splice(i, 1);
     client.disconnect();
   });
 
   client.on('disconnect', () => {
     console.log('user disconnected', client.id);
-    var i = allClients.indexOf(client);
+    const i = allClients.indexOf(client);
     allClients.splice(i, 1);
     client.disconnect();
   });
 
 });
 
-http.listen(3000, () => {
-  console.log('listening on port 3000');
+http.listen(3001, () => {
+  console.log('listening on port 3001');
 });
+
+// Função que verifica se a rota mudou, caso sim desisncreva.
+const verificaRota = (newUrl) => {
+  if(rotaAutal !== null && newUrl !== rotaAutal) {
+    try {
+      subs.unsubscribe();
+    } catch (error) {
+      ;
+    }
+  }
+}
---
+++ services/apiFirebaseAcess.js
@@ -0,0 +1,45 @@
+// import axios from 'axios';
+import { Observable } from 'rxjs/Observable'
+import FirebaseService from './firebase/firebase';
+
+const fireUser = FirebaseService.database().ref().child('rotas');
+
+class ApiFirebaseAcess {
+
+  static getAll() {
+    return Observable.create(obs => {
+      fireUser.on('value', snap => {
+        obs.next(snap.val());
+      },(errorObject) => {
+        obs.error(errorObject);
+      });
+    });
+  }
+
+  static getRotaTexto(url) {
+    const rota = url.split('/').join('-');
+    return new Observable(obs => {
+      fireUser.orderByKey().equalTo(rota).on('value', snap => {
+        obs.next(snap.val());
+      },(errorObject) => {
+        obs.error(false);
+      });
+    });
+  }
+
+  static postRotaTexto(url, texto) {
+    console.log(url)
+    const rota = url.split('/').join('-');
+    return new Promise ((resolve, reject) => {
+      const fire = FirebaseService.database().ref().child(`rotas/${rota}/texto`);
+      fire.set(texto, erro => {
+        if(erro === false)
+          resolve(false);
+      });
+      resolve(true);
+    });
+  }
+
+}// Fim da classe.
+
+export default ApiFirebaseAcess;
